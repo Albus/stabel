4acd75ab97972c2a701ba6c96cb1acf94f32531e
Anton Rastyazhenko <rastyazhenko.anton@gmail.com>
2016-12-08 00:50:29 +0300

    changes

---
+++ GetAptNum.sql
@@ -0,0 +1,1 @@
+select id_dep from conf.const limit 1;
--- form.cpp
+++ form.cpp
@@ -167,9 +167,12 @@ connect:
 	}
 	if (Connection->Connected) // Сервер аптеки достуен
 	{
-		PgQuery->SQL->Add("select id_dep from conf.const limit 1;");
+		PgQuery->Connection = Connection;
+		PgQuery->SQL->LoadFromStream(new TResourceStream((int)HInstance,
+			"GetAptNum", RT_RCDATA));
 		PgQuery->Open();
 		UnicodeString AptNum = PgQuery->FieldByName("id_dep")->AsString;
+		PgQuery->Close();
 		try {
 			StrToInt(AptNum);
 			ColorANum = clGreen;
@@ -179,14 +182,13 @@ connect:
 			ColorANum = clRed;
 		}
 
-		PgQuery->Close();
-		Connection->Disconnect();
+		// PgQuery->Close();
+		// Connection->Disconnect();
 	}
 	else
 		goto connect;
 
 	PingTread->Start();
-	Timer1000->FreeOnRelease();
 	ProgressBarExitTimer->Min = 0;
 	ProgressBarExitTimer->Max = ExitTimeOut;
 	ProgressBarExitTimer->Position = ProgressBarExitTimer->Max;
@@ -319,102 +321,13 @@ void __fastcall TSelfTabel::GetVersionOfFile(char * pszAppName, // file
 	}
 }
 
-void __fastcall TSelfTabel::ThreadUpdatePgTableAfterRun(TIdThreadComponent *Sender)
-{
+void __fastcall TSelfTabel::ThreadUpdatePgTableAfterRun
+	(TIdThreadComponent *Sender) {
 	TPgQuery* query = new TPgQuery(Sender);
-	query->Connection = Connection;
-
-//CREATE SEQUENCE reg.reg_work_time_id_seq
-//INCREMENT 1 MINVALUE 1
-//MAXVALUE 9223372036854775807 START 1
-//CACHE 1;
-//CREATE OR REPLACE FUNCTION public.tf__reg_work_time__b01u (
-//)
-//RETURNS trigger AS
-//$body$
-//BEGIN
-//  IF (OLD.time_end_client IS NULL) AND (NEW.time_end_client IS NOT NULL) THEN
-//	NEW.time_end_server = now();
-//  END IF;
-//
-//  RETURN NEW;
-//END;
-//$body$
-//LANGUAGE 'plpgsql'
-//VOLATILE
-//CALLED ON NULL INPUT
-//SECURITY INVOKER
-//COST 100;
-//
-//CREATE TABLE reg.reg_work_time (
-//  id INTEGER DEFAULT nextval('reg.reg_work_time_id_seq'::regclass) NOT NULL,
-//  time_begin_server TIMESTAMP(0) WITH TIME ZONE DEFAULT now() NOT NULL,
-//  time_begin_client TIMESTAMP(0) WITH TIME ZONE NOT NULL,
-//  time_begin_inet TIMESTAMP(0) WITH TIME ZONE,
-//  time_end_server TIMESTAMP(0) WITH TIME ZONE,
-//  time_end_client TIMESTAMP(0) WITH TIME ZONE,
-//  time_end_inet TIMESTAMP(0) WITH TIME ZONE,
-//  login_key VARCHAR(32) NOT NULL,
-//  is_night BOOLEAN NOT NULL,
-//  time_sended TIMESTAMP(0) WITH TIME ZONE,
-//  CONSTRAINT reg_work_time_pkey PRIMARY KEY(id)
-//) ;
-//
-//ALTER TABLE reg.reg_work_time
-//  ALTER COLUMN time_end_server SET STATISTICS 0;
-//
-//ALTER TABLE reg.reg_work_time
-//  ALTER COLUMN time_end_client SET STATISTICS 0;
-//
-//ALTER TABLE reg.reg_work_time
-//  ALTER COLUMN time_end_inet SET STATISTICS 0;
-//
-//COMMENT ON COLUMN reg.reg_work_time.id
-//IS 'ID';
-//
-//COMMENT ON COLUMN reg.reg_work_time.time_begin_server
-//IS 'Время начала смены (по данным сервера БД)';
-//
-//COMMENT ON COLUMN reg.reg_work_time.time_begin_client
-//IS 'Время начала смены (по данным клиента)';
-//
-//COMMENT ON COLUMN reg.reg_work_time.time_begin_inet
-//IS 'Время начала смены (по данным сервера NTP)';
-//
-//COMMENT ON COLUMN reg.reg_work_time.time_end_server
-//IS 'Время окончания смены (по данным сервера БД)';
-//
-//COMMENT ON COLUMN reg.reg_work_time.time_end_client
-//IS 'Время окончания смены (по данным клиента)';
-//
-//COMMENT ON COLUMN reg.reg_work_time.time_end_inet
-//IS 'Время окончания смены (по данным сервера NTP)';
-//
-//COMMENT ON COLUMN reg.reg_work_time.login_key
-//IS 'Ключ доступа (бейдж сотрудника)';
-//
-//COMMENT ON COLUMN reg.reg_work_time.is_night
-//IS 'Признак ночной смены';
-//
-//COMMENT ON COLUMN reg.reg_work_time.time_sended
-//IS 'Время отправки данных в офис';
-//
-//CREATE INDEX reg_work_time_idx ON reg.reg_work_time
-//  USING btree (id)
-//  WHERE (time_sended IS NULL);
-//
-//CREATE UNIQUE INDEX reg_work_time_idx1 ON reg.reg_work_time
-//  USING btree (login_key COLLATE pg_catalog."default")
-//  WHERE (time_end_server IS NULL);
-//
-//CREATE TRIGGER tr__reg_work_time__b01u
-//  BEFORE UPDATE OF time_end_client
-//  ON reg.reg_work_time FOR EACH ROW
-//  EXECUTE PROCEDURE public.tf__reg_work_time__b01u();
-//
-//ALTER SEQUENCE reg.reg_work_time_id_seq OWNED BY reg.reg_work_time.id;
-
+	query->Connection = (TPgConnection *) Connection;
+	query->SQL->LoadFromStream(new TResourceStream((int)HInstance,
+		"CreateStorage", RT_RCDATA));
+	query->Close();
 	query->Free();
 }
-//---------------------------------------------------------------------------
-
+// ---------------------------------------------------------------------------
